98ad7e49b2cf658a44844e3695e997c0cfc016e2
CrashLogger <erlantz.alonso@live.com>
2022-08-24 23:42:20 +0200

    Precision update 3
    I finally did something useful and made the car control more precise. I feel like 64 is a low enough minimum duty cycle but if you have a real L298N instead of my cheap copy you can probably go lower.

--- CarReceiver/CarReceiver.ino
+++ CarReceiver/CarReceiver.ino
@@ -1,4 +1,4 @@
-  /*
+   /*
  * 
  * An NRF24L01 based RC receiever for L298N based 4 wheel or two wheel drive vehciles with tank steering
  * A standard short pcb laid antenna NRF24L01 is used on this side
@@ -9,6 +9,9 @@
  * Erlantz 'CrashLogger' Alonso, 2021
  * 
 */
+
+//To go forward       Left side: 6H 9L                Right side: 4H 5L
+//To go backward      Left side: 6L 9H                Right side: 4L 5H
 
 #include <SPI.h>
 #include "printf.h"
@@ -29,19 +32,18 @@ RF24 radio(7, 8); // using pin 7 for the CE pin, and pin 8 for the CSN pin
 uint8_t address[][6] = {"000FA", "000FF"};    //REC, ACK
 bool radioNumber = 1; //Receiver will use pipe [1] to transmit acknowledgements
 
-  
-  int powerR = 4;
-  int powerL = 4;
+int PowerR = 0;
+int PowerL = 0;
 
+int Margin = 64;
+  
 void setup() {
 
   // Initialise the SPI bus transciever
   if (!radio.begin()) {
     ////Serial.println(F("radio hardware is not responding!!"));
-    while (1) {} // hold in infinite loop if it can't connect to the controller
+    while (1) {} // Hold in infinite loop if it can't connect to the controller
   }
-
-  Serial.begin(9600);
 
   //radio.enableAckPayload();
 
@@ -62,67 +64,59 @@ void loop() {
     if (radio.available(&pipe)) {                 // Check if there's a package waiting
       uint8_t bytes = radio.getPayloadSize();     // Read the package size
       radio.read(&data, bytes);                   // Read the package
-      powerL = map(data.thrust, 0, 1023, 9, 0);
-      powerR = map(data.elevator, 0, 1023, 9, 0);
       digitalWrite(2, data.buzzer);
    }
-  digitalWrite(2, data.buzzer);
-  Serial.println(powerL);
-  Right();
+  Power();
+}
+
+void Power(){
   Left();
+  Right();
+  if (PowerL>255){
+    PowerL = 255;
+  }
+
+  if (PowerR > 255){
+    PowerR = 255;
+  }
+
+  analogWrite(3, PowerR);
+  analogWrite(10, PowerL);
+}
+
+void Left(){
+  if (data.thrust>((1024/2)+Margin)){
+    digitalWrite(6, HIGH);
+    digitalWrite(9, LOW);
+    PowerL = map(data.thrust, (1024/2)+Margin, 1024, 64, 255);
+  }
+  else if (data.thrust<((1024/2)-Margin)){
+    digitalWrite(6, LOW);
+    digitalWrite(9, HIGH);
+    PowerL = map(data.thrust, (1024/2)-Margin, 24, 64, 255);
+  }
+  else{
+    digitalWrite(6, LOW);
+    digitalWrite(9, LOW);
+    PowerL = 0;
+  }
 }
 
 void Right(){
-  digitalWrite(2, data.buzzer);
-  if (powerR > 6){
+  if (data.elevator>((1024/2)+Margin)){
+    digitalWrite(4, HIGH);
+    digitalWrite(5, LOW);
+    PowerR = map(data.elevator, (1024/2)+Margin, 1024, 64, 255);
+  }
+  else if (data.elevator<((1024/2)-Margin)){
     digitalWrite(4, LOW);
     digitalWrite(5, HIGH);
-    if (powerR > 8){
-      analogWrite(3, 255);
-    }
-    else{
-      analogWrite(3, 132);
-    }
-  }
-  else if (powerR < 3){
-    digitalWrite(5, LOW);
-    digitalWrite(4, HIGH);
-    if (powerR <  1){
-      analogWrite(3, 255);
-    }
-    else{
-      analogWrite(3,132);
-    }
+    PowerR = map(data.elevator, (1024/2)-Margin, 24, 64, 255);
   }
   else{
     digitalWrite(4, LOW);
     digitalWrite(5, LOW);
+    PowerR = 0;
   }
-}
-void Left(){
-  digitalWrite(2, data.buzzer);  
-  if (powerL > 6){
-    digitalWrite(6, LOW);
-    digitalWrite(9, HIGH);
-    if (powerL >= 7){
-      analogWrite(10, 255);
-    }
-    else{
-      analogWrite(10, 132);
-    }
-  }
-  else if (powerL < 3){
-    digitalWrite(9, LOW);
-    digitalWrite(6, HIGH);
-    if (powerL <= 1){
-      analogWrite(10, 255);
-    }
-    else{
-      analogWrite(10,132);
-    }
-  }
-  else{
-    digitalWrite(9, LOW);
-    digitalWrite(6, LOW);
-  }
+  
 }
